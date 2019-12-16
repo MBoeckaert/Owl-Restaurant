6c8efe68ae97220898d1f68a930ea02b5766c6f1
EsportsGrandpa <matthiasboeckaert@gmail.com>
2019-12-16 09:31:35 +0100

    add js

---
+++ Eindopdracht-frontend/project-start/script.js
@@ -0,0 +1,26 @@
+window.addEventListener('scroll', function (e) {
+    var header = document.querySelector('.navigation');
+    var sticky = window.scrollY;
+
+    if (sticky >= 30) {
+        header.classList.add("sticky");
+        console.log('50');
+    }
+    else {
+        header.classList.remove("sticky");
+        console.log('scrolled!');
+    }
+});
+
+
+const showNav = () => {
+    const burger = document.querySelector('.hamburger');
+    const dropMenu = document.querySelector('.dropdownMenu');
+
+    burger.addEventListener('click', () => {
+        dropMenu.classList.toggle('dropActive');
+        console.log('clicke');
+    });
+}
+
+showNav();
