a4a0aabc1ca04d4efd84b803e27878a14e0171d6
EsportsGrandpa <matthiasboeckaert@gmail.com>
2019-12-18 10:52:04 +0100

    cleanup code

--- Eindopdracht-frontend/project-start/script.js
+++ Eindopdracht-frontend/project-start/script.js
@@ -5,25 +5,15 @@ window.addEventListener("scroll", function (e) {
     sticky >= 40 ? header.classList.add("sticky") : header.classList.remove("sticky")
 });
 
-// const burger = document.querySelector('.hamburger');
-// burger.addEventListener('click', function (e) {
-//     alert('test');
-//     burger.preventDefault();
-// })
-
-
 const showNav = () => {
     console.log('showNav');
 
     const burger = document.querySelector('.hamburger');
     const dropMenu = document.querySelector('.dropdownMenu');
-    console.log('dropMenu', dropMenu);
-
 
     burger.addEventListener('click', (e) => {
         console.log('burger');
         dropMenu.classList.toggle('dropActive');
-        // e.preventDefault();
     })
 }
 
